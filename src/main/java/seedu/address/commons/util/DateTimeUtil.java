34ce2097afdd877a988be661165f7bb6a99a18bf
holmesjj <hujiajun19961024@gmail.com>
2021-10-13 01:34:52 +0800

    Update find features (find by id, name, email. phone and registration date)

---
+++ src/main/java/seedu/address/commons/util/DateTimeUtil.java
@@ -0,0 +1,22 @@
+package seedu.address.commons.util;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Helper functions for handling dates and times.
+ */
+public class DateTimeUtil {
+
+    private static final long ONE_DAY_MILLISECONDS = 24 * 60 * 60 * 1000;
+
+    /**
+     * Returns true if the {@code date} contains the {@code timestamp}.
+     */
+    public static boolean isDateContainsTimestamp(Date date, long timestamp) {
+        Timestamp dateToTimestamp = new Timestamp(date.getTime());
+        long start = dateToTimestamp.getTime();
+        long end = dateToTimestamp.getTime() + ONE_DAY_MILLISECONDS;
+        return timestamp >= start && timestamp < end;
+    }
+}
--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,6 +1,12 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REGISTRATION_DATE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
@@ -9,7 +15,11 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.member.EmailContainsKeywordsPredicate;
+import seedu.address.model.member.IdContainsKeywordsPredicate;
 import seedu.address.model.member.NameContainsKeywordsPredicate;
+import seedu.address.model.member.PhoneContainsKeywordsPredicate;
+import seedu.address.model.member.RegistrationDateContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -17,18 +27,72 @@ public class FindCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_validIdArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new IdContainsKeywordsPredicate(Arrays.asList("10001", "10002")));
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_ID + " 10001 10002",
+                expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_ID + " \n 10001 \n \t 10002  \t",
+                expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validNameArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-        assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_NAME + " Alice Bob",
+                expectedFindCommand);
 
         // multiple whitespaces between keywords
-        assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_NAME + " \n Alice \n \t Bob  \t",
+                expectedFindCommand);
     }
 
+    @Test
+    public void parse_validPhoneArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new PhoneContainsKeywordsPredicate(Arrays.asList("87328807", "99272758")));
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_PHONE + " 87328807 99272758",
+                expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_PHONE
+                + " \n 87328807 \n \t 99272758  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validEmailArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new EmailContainsKeywordsPredicate(Arrays.asList("ben@gmail.com", "sunny@gmail.com")));
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_EMAIL + " ben@gmail.com sunny@gmail.com",
+                expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_EMAIL
+                + " \n ben@gmail.com \n \t sunny@gmail.com  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validRegistrationDateArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand = new FindCommand(new RegistrationDateContainsKeywordsPredicate(
+                Arrays.asList("2021-01-01", "2021-01-02")));
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_REGISTRATION_DATE
+                + " 2021-01-01 2021-01-02", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " " + PREFIX_MEMBER + " " + PREFIX_REGISTRATION_DATE
+                + " \n 2021-01-01 \n \t 2021-01-02  \t", expectedFindCommand);
+    }
 }
