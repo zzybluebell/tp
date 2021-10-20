c02d83508e559ad29de063edb3e948a6d3c24c7c
Zhang Zhiyao <zzy1007102679@gmail.com>
2021-10-20 18:27:12 +0800

    Update minor change of abstract class of AddCommandParser

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,9 +6,6 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new AddMemberCommand object
  */
-public abstract class AddCommandParser implements Parser<AddMemberCommand> {
-    private static final String ID_STUB = "00001";
-    private static final String REGISTRATION_TIMESTAMP_STUB = "1609459200000";
-
+public abstract class AddCommandParser {
     public abstract AddMemberCommand parse(String args) throws ParseException;
 }
