4fcec5cdc0e338176e3784805db3a599148513bb
Yang Yuzhao <yang.yuzhao@dhs.sg>
2021-10-20 22:08:04 +0800

    Wrap up the abstraction of AddTransaction logic and implementation.

--- src/main/java/seedu/address/logic/parser/AddCommandPrefixParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandPrefixParser.java
@@ -1,14 +1,42 @@
 package seedu.address.logic.parser;
 
+import seedu.address.commons.status.ExecutionStatus;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TRANSACTION;
 
+/**
+ * Distinguishes between add member and add transaction commands and
+ * returns an AddMemberCommandParser or AddTransactionCommandParser
+ * depends on the very first prefix appears after command word.
+ */
 public class AddCommandPrefixParser {
+	private final Model model;
+	private final ExecutionStatus executionStatus;
 
+	/**
+	 * Constructor for AddCommandPrefixParser.
+	 * @param model
+	 * @param executionStatus
+	 */
+	public AddCommandPrefixParser(Model model, ExecutionStatus executionStatus) {
+		this.model = model;
+		this.executionStatus = executionStatus;
+	}
+
+	/**
+	 * Parses the arguments depends on the first prefix.
+	 * @param args
+	 * @return AddMemberCommandParser or AddTransactionCommandParser.
+	 * @throws ParseException
+	 */
 	public AddCommandParser parse(String args) throws ParseException {
 		String trimmedArgs = args.trim();
+		String prefix;
 		if (trimmedArgs.isEmpty()) {
 			throw new ParseException(
 					String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
@@ -16,9 +44,18 @@ public class AddCommandPrefixParser {
 
 		int index = trimmedArgs.indexOf(' ');
 		if (index > -1) { // Check if there is more than one word.
-			return trimmedArgs.substring(0, index).trim(); // Extract first word.
+			prefix = trimmedArgs.substring(0, index).trim(); // Extract first word.
 		} else {
-			return trimmedArgs; // Extracted word is the first word itself.
+			prefix = trimmedArgs; // Extracted word is the first word itself.
+		}
+
+		if (prefix.equals(PREFIX_MEMBER.getPrefix())) {
+			return new AddMemberCommandParser(model, executionStatus);
+		} else if (prefix.equals(PREFIX_TRANSACTION.getPrefix())) {
+			return new AddTransactionCommandParser(model, executionStatus);
+		} else {
+			throw new ParseException(
+					String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
 		}
 	}
 }
