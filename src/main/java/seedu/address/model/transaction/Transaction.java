ec25089bdc849342b6a3f65a8085a6746d9d96d1
Zhang Zhiyao <zzy1007102679@gmail.com>
2021-10-25 11:53:47 +0800

    Fix transaction enabled duplication bugs

--- src/main/java/seedu/address/model/transaction/Transaction.java
+++ src/main/java/seedu/address/model/transaction/Transaction.java
@@ -49,9 +49,13 @@ public class Transaction {
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Transaction // instanceof handles nulls
-                && transactionAmount.equals(((Transaction) other).transactionAmount)); // state check
+        if (other instanceof Transaction) {
+            return false;
+        } else {
+            return other == this // short circuit if same object
+                    || (other instanceof Transaction // instanceof handles nulls
+                    && transactionAmount.equals(((Transaction) other).transactionAmount)); // state check
+        }
     }
 
     //todo: can use the id to generate hashcode
