3fed8b140483d2fde859ea8ccbe84ea2f4e3f6ee
Yang Yuzhao <yang.yuzhao@dhs.sg>
2021-10-27 23:10:14 +0800

    Update basic frontend support for pop-up ViewWindow.
    Let's:
    - fix a bug that memberList is updated unintentionally
    - beautify layout

--- src/main/java/seedu/address/ui/MemberViewWindow.java
+++ src/main/java/seedu/address/ui/MemberViewWindow.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
@@ -46,9 +47,6 @@ public class MemberViewWindow extends UiPart<Stage> {
             }
         }
     }
-
-
-
 
     /**
      * Shows the MemberView window.
